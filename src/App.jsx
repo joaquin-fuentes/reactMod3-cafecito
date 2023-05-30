24d77c957c38f0590bfcbd91bf3d0eddc22a9cde
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2023-05-30 08:48:24 -0300

    se creo estructura principal del proyecto y se instalo dependencias

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return (
+    <div>
+      Hola mundo
+    </div>
+  );
+};
+
+export default App;
