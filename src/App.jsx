54f548c04e3e4cfd7f4f1ee53c48dfe67bb7060b
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2023-05-30 09:04:05 -0300

    Footer completo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import "./App.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Menu from './components/Menu';
+import Footer from './components/Footer';
+import {Container} from "react-bootstrap"
 
 const App = () => {
   return (
     <>
       <Menu></Menu>
+      <Container className='mainPage'></Container>
+      <Footer></Footer>
     </>
   );
 };
