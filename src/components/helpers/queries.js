9bad819d3f887af92b6344f39184b7701a8a26a2
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2023-06-13 16:51:36 -0300

    Agregamos la consulta para cerar un producoty resetar el formulario de crearproducto

--- src/components/helpers/queries.js
+++ src/components/helpers/queries.js
@@ -58,3 +58,20 @@ export const consultaBorrarProducto = async (id)=>{
     }
 }
 
+export const consultaCrearProducto = async (producto)=>{
+    try {
+        const respuesta = await fetch(URL_PRODUCTO, {
+            method: "POST",
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify(producto)
+        });
+        return respuesta
+
+    } catch (error) {
+        console.log(error)
+        return null
+    }
+}
+
