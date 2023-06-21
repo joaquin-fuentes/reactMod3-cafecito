05794ab7b94961d5c1f5e3a32bbcdc39c396d620
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2023-06-21 09:12:35 -0300

    cambios en tabla de admnistrador de productos y agregue las options al editarProducto

--- src/components/views/producto/ItemProducto.jsx
+++ src/components/views/producto/ItemProducto.jsx
@@ -42,7 +42,6 @@ const ItemProducto = ({ producto, setProductos }) => {
             <td>{producto.id}</td>
             <td>{producto.nombreProducto}</td>
             <td>{producto.precio}</td>
-            <td>{producto.imagen}</td>
             <td>{producto.categoria}</td>
             <td className='text-center'>
                 <Link to={`/administrador/editar/${producto.id}`} className={"btn btn-warning m-1"} >Editar</Link>
